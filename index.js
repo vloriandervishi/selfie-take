fb294625f726a684aa659fa2f5fa5984edab6551
vlorian <vlorian.dervishi@gmail.com>
2023-07-15 20:27:56 -0400

    save req.body into data in order to acess lon and lat

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ app.use(express.static("./Public"));
 app.use(express.json({ limit: "1mb" }));
 app.post("/api", (req, res) => {
   console.log(req.body);
+  const data = req.body;
+  res.json({ status: "success", latitude: data.lat, longitude: data.lon });
 });
