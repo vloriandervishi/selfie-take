689ac038d5637b4088e0100e730488960114ee06
vlorian <vlorian.dervishi@gmail.com>
2023-07-16 14:05:33 -0400

    nedb

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const database = require("nedb");
 app.listen(3000, () => console.log("Server is running on port"));
-
 app.use(express.static("./Public"));
 app.use(express.json({ limit: "1mb" }));
+const db = new database("store.db");
+db.loadDatabase();
+app.get("/api", (req, res) => {
+  db.find({}, (err, docs) => {
+    if (err) {
+      res.end();
+      return;
+    }
+    res.send(docs);
+  });
+});
 app.post("/api", (req, res) => {
-  console.log(req.body);
   const data = req.body;
-  res.json({ status: "success", latitude: data.lat, longitude: data.lon });
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  db.insert(data);
+  res.json({
+    status: "success",
+    latitude: data.lat,
+    timestamp: timestamp,
+    longitude: data.lon,
+  });
 });
